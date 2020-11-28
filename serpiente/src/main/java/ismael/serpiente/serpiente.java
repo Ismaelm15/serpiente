082c99afa5a39d60fe2984bb542bc346b6a5a76e
M15 <ismaelmelgar1999@gmail.com>
2020-11-28 11:43:07 +0100

    Hay que cambiarlo entero v2

--- serpiente/src/main/java/ismael/serpiente/serpiente.java
+++ serpiente/src/main/java/ismael/serpiente/serpiente.java
@@ -13,41 +13,53 @@ import java.util.Scanner;
  */
 public class serpiente {
 
+    static Scanner ser = new Scanner(System.in);
+    static int k = 0, mediciones, comienzo = 0, comienzomax = 0, duracion = 0, duracionmax = 0;
+
+    static int nmediciones[] = null;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner ser = new Scanner(System.in);
-        int k = 0, mediciones,comienzo=0,duracion=0,duracionmax=0;
 
-        int nmediciones[] = null;
+        mediciones();
+        consecutiva();
+        array(mediciones);
+        for (int j = 0; j < nmediciones.length; j++) {
+            if (nmediciones[j] == 1 && duracion == 0) {
+                comienzo = j;
+                
+            } else if () {
+            }
+        }
+
+    }
+
+    public static void mediciones() {
+
         System.out.println("Introduzca el numero de mediciones");
         mediciones = ser.nextInt();
+        
+    }
+
+    public static void consecutiva() {
         System.out.println("Introduce número de mediciones consecutivas por debajo del umbral(k)");
         k = ser.nextInt();
-        nmediciones = new int[mediciones];
-        System.out.println("Introduzca las " + mediciones + " mediciones");
+         
+    }
+
+   public static void array(int medi){
+   nmediciones = new int[medi];
+        System.out.println("Introduzca las " + medi + " mediciones");
         for (int i = 0; i < nmediciones.length; i++) {
             nmediciones[i] = ser.nextInt();
             if (nmediciones[i] != 1 && nmediciones[i] != 0) {
                 i--;
                 System.out.println("Ha introducido un numero erroneo intentelo de nuevo");
             }
-        }
-        for (int j=0;j<nmediciones.length;j++){
-        if (nmediciones[j]==1){ 
-            duracion++;
-        }else{duracion=0;}
-        
-        if (duracionmax<duracion){
-            comienzo=duracion-j;
-        duracionmax=duracion;
-        }
-        
-        
-        
-        }
-        
-    }
-
+   }
+       
 }
+}
+        
